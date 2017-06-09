0624367313c91af14850a640c4377cd15132b325
Andreas <Andreas@Andreas-Pc>
2017-06-09 02:16:28 +0200

    Rule Classes added

--- CDIOFinal/src/logic/RuleSetInterface.java
+++ CDIOFinal/src/logic/RuleSetInterface.java
@@ -1,5 +1,4 @@
 package logic;
-import java.util.Set;
 import java.util.function.Predicate;
 
 
@@ -8,11 +7,36 @@ public interface RuleSetInterface {
 	static final int minName = 2, maxName = 22;
 	static final int minIni = 2, maxIni = 5;
 	static final int minPwd = 6, minPwdSymbols = 3, maxPwd = 30;
-	static final double minNonNetto = 0.05, maxNonNetto = 20.0;
+	static final double minNomNetto = 0.05, maxNomNetto = 20.0;
 	static final double minTolerance = 0.1, maxTolerance = 10.0;
-	static final int minStatus = 0, maxStatus = 2;
-	static final int cpr = 10;
-	
-	
-	
+	static final int minStatus = 0, midStatus = 1, maxStatus = 2;
+
+	Rule<Integer> getID();
+	Rule<Integer> getStatus();
+	Rule<Double> getNomNetto();
+	Rule<Double> getTolerance();
+	Rule<String> getName();
+	Rule<String> getIni();
+	Rule<String> getCpr();
+	Rule<String> getPwd(); 
+//	Rule<Set<String>> getRole();
+
+	class Rule<T> {
+		String message;
+		Predicate<T> pred;
+
+		public Rule(String message, Predicate<T> pred){
+			this.message = message;
+			this.pred = pred;
+		}
+
+		@Override
+		public String toString() {
+			return message;
+		}
+
+		public boolean test(T t) {
+			return pred.test(t);
+		}
+	}
 }	
